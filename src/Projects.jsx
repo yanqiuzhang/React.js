8e979d1d79dc16bacb170a566a10286a16fb3611
olivia <olivia@evolve.se>
2019-02-08 15:04:02 +0100

    add css and projects link

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import axios from "axios";
 import ProjectCard from "./ProjectCard";
+import Link from 'next/link';
 
 class Projects extends Component {
   constructor() {
@@ -32,10 +33,62 @@ class Projects extends Component {
         )
       })
     }
+
     return (
       <div>
+        <div className="title-div">
+          {projectsList}
+        </div>
+        <div className="Project-dev">
         <h1 className="content-text">My Projects</h1>
-        {projectsList}
+         <div className="Project-dev2">
+          <ul>
+            <li><Link href="https://github.com/yanqiuzhang/fizzbuzz_js_challenge"><a>FizzBuzz</a></Link></li>
+            <li><Link href="https://github.com/yanqiuzhang/Address_Book"><a>Address Book</a></Link></li>
+            <li><Link href="https://github.com/yanqiuzhang/BMI_challenge"><a>BMI</a></Link></li>
+          </ul>
+         </div>
+        </div>
+
+        <style jsx>{`
+        .content-text{
+          text-align: center;
+          padding: 20px;
+          font-style: italic;
+          font-family: "Times New Roman", Times, serif;
+          color: #4c9bf1;
+          font-size: 40px;
+        }
+        a{
+          font-style: italic;
+          color: #4c9bf1;
+          font-size: 24px;
+          text-decoration: none;
+          font-family: "Times New Roman", Times, serif;
+          z-index: 1;
+        }
+        a:hover {
+          color: blue;
+        }
+        li{
+          padding-top: 17px;
+        }
+        .Project-dev{
+          margin: 0 auto;
+        }
+        .Project-dev2{
+          width: 100%;
+          height: 350px;
+          background: url(http://www.europa-re.com/uploads/pb2.png) no-repeat center center;
+          background-size: 100% 100%;
+          position: relative;
+          float: left;
+          left: 5px;
+          margin: 0;
+          opacity: 0.7;
+          object-fit:cover;
+        }
+        `}</style>
       </div>
     )
   };
